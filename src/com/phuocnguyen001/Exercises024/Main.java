9e93829ca10eb5149013b17c24c0cf44169c4b6d
pnguyen215 <nguyenhuuphuocit97@gmail.com>
2019-04-18 14:09:58 +0700

    the third commit

--- src/com/phuocnguyen001/Exercises024/Main.java
+++ src/com/phuocnguyen001/Exercises024/Main.java
@@ -1,8 +1,14 @@
 package com.phuocnguyen001.Exercises024;
 
 public class Main {
-
-	public static void main(String[] args) {
+	public static void main(String[] argv) throws Exception {
+		String data = "java2s.com";
+		System.out.println(data);
+		int a = 10;
+		int b = 17;
+		int c = a + b;
+		System.out.println(c);
+// hello
 
 	}
 
--- src/com/phuocnguyen001/Exercises040/Main.java
+++ src/com/phuocnguyen001/Exercises040/Main.java
@@ -3,7 +3,8 @@ package com.phuocnguyen001.Exercises040;
 public class Main {
 
 	public static void main(String[] args) {
-
+		System.out.println("Hello Phuoc Nguyen");
+		
 	}
 
 }
