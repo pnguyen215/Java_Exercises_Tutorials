95784c8f6db8ec82e54a96aaaa92439e3e9123f5
pnguyen215 <nguyenhuuphuocit97@gmail.com>
2019-09-08 18:03:47 +0700

    the fifth commit

--- src/com/phuocnguyen001/Exercises025/Main.java
+++ src/com/phuocnguyen001/Exercises025/Main.java
@@ -2,8 +2,8 @@ package com.phuocnguyen001.Exercises025;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	}
+    }
 
 }
---
+++ src/com/phuocnguyen001/Exercises025/Service.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.phuocnguyen001.Exercises025;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author David Nguyen
+ */
+public class Service implements Serializable {
+
+    public Service() {
+
+    }
+
+    public void method() {
+        // to do something
+
+    }
+}
